a59c6d2548b4da70781d74d8c2b6541e24b6e99a
Hannah Oberlohr <hannahoberlohr@gmail.com>
2017-12-06 12:04:43 -0600

    Class 3.4 prep video - Views/razor templates

---
+++ NewCheese/Controllers/CheeseController1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewCheese.Controllers
+{
+    public class CheeseController1 : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<string> cheeses = new List<string>();
+
+            cheeses.Add("Cheddar");
+            cheeses.Add("Provel");
+            cheeses.Add("Pepperjack");
+
+            ViewBag.cheeses = cheeses;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
