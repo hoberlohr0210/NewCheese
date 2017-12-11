a90aa0c957cdb0e36d46c5e884db53249a12c6ec
Hannah Oberlohr <hannahoberlohr@gmail.com>
2017-12-11 13:46:22 -0600

    Added in Ids and Remove methods

--- NewCheese/Controllers/CheeseController1.cs
+++ NewCheese/Controllers/CheeseController1.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using NewCheese.Models;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace NewCheese.Controllers
 {
     public class CheeseController : Controller
-    {
-        static private List<string> Cheeses = new List<string>();
+    {       
         // GET: /<controller>/
         public IActionResult Index()
         {
-            
-            ViewBag.cheeses = Cheeses;
-
+            ViewBag.cheeses = CheeseData.GetAll();
             return View();
         }
 
@@ -29,10 +26,34 @@ namespace NewCheese.Controllers
         [Route("/Cheese/Add")] //specify where the form lives
                                 // our form has no action method,
                                 //so we need this here
-        public IActionResult NewCheese(string name)
+        public IActionResult NewCheese(string name, string description)
+        {          
+            //accessing the empty constructor i created in the Cheese model class
+            Cheese newCheese = new Cheese
+            {
+                Description = description,
+                Name = name
+            };
+            //Add the new cheese to my existing cheeses by passing it to the Add method
+            CheeseData.Add(newCheese);
+            return Redirect("/Cheese");
+        }
+
+        [HttpGet]
+        public IActionResult Remove()
         {
-            //Add new cheese to my existing cheeses
-            Cheeses.Add(name);
+            ViewBag.cheeses = CheeseData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Remove")]
+        public IActionResult Remove(int[] cheeseIds)
+        {
+            foreach (int cheeseId in cheeseIds)
+            {
+                CheeseData.Remove(cheeseId);            
+            }
 
             return Redirect("/Cheese");
         }
--- NewCheese/Startup.cs
+++ NewCheese/Startup.cs
@@ -43,7 +43,7 @@ namespace NewCheese
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Cheese}/{action=Index}/{id?}");
             });
         }
     }
